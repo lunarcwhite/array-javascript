7c3470f59f7a8290368a989ec7e37657982dabe8
lunarcwhite <rizkym058@gmail.com>
2023-03-07 21:22:48 +0700

    tugas praktek array

---
+++ tugas_praktek1.js
@@ -0,0 +1,7 @@
+let mamalia = ['harimau', 'singa', 'kucing', 'kelinci', 'kelelawar'];
+let unggas = ['ayam', 'burung pipit', 'elang', 'angsa', 'puyuh'];
+let reptil = ['ular', 'kadal', 'iguana', 'biawak', 'komodo'];
+
+console.log(mamalia[1], mamalia[3])
+console.log(unggas[1], unggas[3])
+console.log(reptil[1], reptil[3])
---
+++ tugas_praktek2.js
@@ -0,0 +1,14 @@
+let mamalia = ['harimau', 'singa', 'kucing', 'kelinci', 'kelelawar'];
+let unggas = ['ayam', 'burung pipit', 'elang', 'angsa', 'puyuh'];
+let reptil = ['ular', 'kadal', 'iguana', 'biawak', 'komodo'];
+
+let binatang = [mamalia, unggas, reptil]
+console.log(binatang[0][0])
+console.log(binatang[0][2])
+console.log(binatang[0][4])
+console.log(binatang[1][0])
+console.log(binatang[1][2])
+console.log(binatang[1][4])
+console.log(binatang[2][0])
+console.log(binatang[2][2])
+console.log(binatang[2][4])
